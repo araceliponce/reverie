94d66077fe8addf1071b8c99ea1ff38781b83d93
araceliponce <113866541+araceliponce@users.noreply.github.com>
2022-11-28 08:41:11 -0500

    corchetes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
 let codeBlocks = document.querySelectorAll('code');
+
 codeBlocks.forEach(c=>{
   console.log(c.outerHTML);
+
   c.addEventListener ('click',()=>{
     navigator.clipboard.writeText(c.outerHTML);
+  })
 });
 
