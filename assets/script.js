b5e5914dbafa159cec85e16fb12eae50341b65cc
arclps <aponcesanga@gmail.com>
2022-11-28 11:16:35 -0500

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
 let codeBlocks = document.querySelectorAll('code');
 
-codeBlocks.forEach(c=>{
-  console.log(c.outerHTML);
+codeBlocks.forEach(c => {
+  console.log(c);
 
-  c.addEventListener ('click',()=>{
+  /* c.addEventListener ('click',()=>{
     navigator.clipboard.writeText(c.outerHTML);
-  })
+  }) */
 });
+
+
+
+codeBlocks.forEach(c => {
+  let copyButton = document.createElement('button');
+  copyButton.ariaLabel = 'Copiar bloque de código al portapepeles';
+  copyButton.innerText = 'Copiar';
+  c.appendChild(copyButton);
+
+  c.addEventListener('click', () => {
+    let code = c.querySelector('code').innerText;
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerText = 'Copiado'
+
+    setTimeout(() => {
+      copyButton.innerText = 'Copiar'
+    }, 3000);
+  })
+
+})
 
